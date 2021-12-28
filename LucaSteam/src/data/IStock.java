08d7d3bcbed924d6365930a2f256f1b1ab72fdc9
alumno <alumno@Alumno10>
2021-12-28 19:31:40 +0100

    # WARNING: head commit changed in the meantime
    Merge branch 'Gonzalo' of https://github.com/mrlocky97/LucaSteam.git
    into Gonzalo Warning

--- LucaSteam/src/data/IStock.java
+++ LucaSteam/src/data/IStock.java
@@ -1,7 +1,28 @@
 package data;
+
+
+import model.Game;
+
 /*
  * Aqui incluimos los metodos vacios, ya que SOLO ES UN INTERFACE.
  */ 
 public interface IStock {
+	
+	public void addGame(Integer key, Game item) /*throws StockException*/;   
+	public void remove(Integer key) /*throws StockException*/;
+	public void printGames();  
+	public void printPublishers();
+	public void printYearsEven();
+	public void printXXcentury();
+	public void printByGenre(Genre genre);
+	public void printByNintendo(Platform platform);
+	public boolean isBusy();
+    public boolean gameExists(Game item);
+    public boolean gameExists(int key);    
+    
+    //public void serializarStock(String fichero);
+    //public void deSerializarStock(String fichero);
+    
+    //public void CasosDePrueba();
 
 }
