e237f83923ccd6a6b49b123159aa5ccfa2f5b896
alumno <alumno@Alumno03>
2021-12-28 19:13:02 +0100

    Gui-- Interfaz de seleccion de menu
    Services -- Capa SuperService
    Services -- Capa ISuperService
    
    Controller -- User de control

--- LucaSteam/src/controller/User.java
+++ LucaSteam/src/controller/User.java
@@ -1,9 +1,100 @@
 package controller;
 
+import gui.Menu;
 
-/*  Aqui ejecutaremos como USUARIO seleccionando una opcion del menu o GUI
- * Pondemos los switch (que cada opcion nos llevara a una opcion de servicios)   
- */
+
 public class User {
+
+	private ISuperService services = new SuperServices();
+	/**
+	*@param:none
+	*return: void
+	*/
+    public void StartListGames() {
+        boolean seguir = true;
+        do {
+            Menu.showMenu(); //Muestra el menu
+            seguir = this.chooseOptions(); //Entra dentro del metodo de abajo. El switch
+        } while (seguir);
+        System.out.println("   --- Fin del Programa ---");
+    }
+    
+    /**
+	*@param:none
+	*return: void
+	*/
+    public boolean chooseOptions() {
+
+        boolean continuar = true;
+
+        try {
+            switch (Datos.ScanInt()) {//Lee un dato de tipo Int
+                case 1:
+                    //Cargar archivo en el programa y generar List (collection)
+                    services.ChargeList();
+                    break;
+
+                case 2:
+                    //Añadir un juego a la List
+                    services.AddGame();
+                    break;
+
+                case 3:
+                    //Mostrar la Lista de Juegos completa.
+                    services.PrintGames();
+                    break;
+
+                case 4:
+                    //Mostrar la Lista de Juegos filtrados por Nombre
+                    services.ShowGameFilterName();
+                    break;
+
+                case 5:
+                	//Mostrar la Lista de Juegos filtrados por Plataforma
+                    services.ShowGameFilterPlatform();
+                    break;
+
+                case 6:
+                	//Mostrar la Lista de Juegos filtrados por Año
+                    services.ShowGameFilterYear();
+                    break;
+
+                case 7:
+                	//Mostrar la Lista de Juegos filtrados por Genero
+                    services.ShowGameFilterGenre();
+                    break;
+
+                case 8:
+                	//Mostrar la Lista de Juegos filtrados por Editor
+                    services.ShowGameFilterPublisher();
+                    break;
+                    
+                case 9:
+                	//Actualizar una categoria de un juego
+                    services.UpdateGame();
+                    break;
+                   
+                case 10:
+                    //Borrar un Juego
+                    services.DeleteGame();
+                    break;
+                    
+                  //Guardar y salir
+                case 0:
+                	service.SaveList();
+                    continuar = exit();
+                    break;
+            }
+        } catch (Exception e) {
+            System.out.println("error: " + e.toString());
+        }
+        return continuar;
+    }
+
+    private boolean exit() throws Exception {//ScanString lee un dato por Scanner tipo String
+        String sino = Datos.ScanString("   Estas seguro de que desea guardar y salir? (S/N)");
+        return (sino.toUpperCase().charAt(0) != 'S'); //La cambia a mayuscula
+    }
+
 
 }
--- LucaSteam/src/services/SuperServices.java
+++ LucaSteam/src/services/SuperServices.java
@@ -1,5 +1,98 @@
 package services;
 
-public class SuperServices {
+import data.IStock;
+import data.Stock;
+import exceptions.StockExceptions;
+import model.Game;
+import utilidades.Datos;
+
+public class SuperServices implements ISuperServices {
+	
+	private IStock stock = new Stock();
+	
+	/*
+	*@return: Un Objeto de tipo Game
+	*
+	*/
+	public boolean AddGame() throws StockException {
+        Game g = new Game();
+        g.createGame();//Comprobar nombre del metodo en la clase Game
+        return this.AddGame(g);
+    }
+	
+	/*
+	*@param: Game g
+	*
+	*@return: Genera un codigo aleatorio (hash) para un objeto
+	*
+	*/
+	
+    public boolean AddGame(Game g) throws StockException  {
+        //Habria que decidir un algoritmo de cÃ³digos (una funciona Hash)
+        return stock.AddGame((int) (Math.random() * 100000), g);
+    }
+	
+    /*
+	*@return: Borra un Juego de la Lista llamando al metodo para borrar juego. Controla Excepciones de 
+	* posible variable vacía.
+	*/
+	
+	 public boolean deleteGame() throws StockException, Exception {
+	        String name = Datos.ScanString("   Indica el nombre del Juego a Eliminar");
+
+	        if (stock.existsGame(name)) {
+	            stock.deleteGame(name);
+	            System.out.println("   Juego eliminado");
+	            return true;
+	        } else {
+	        	throw new StockException("No existe ese juego en el listado.");
+	            //return false;
+	        }
+	    }
+	 
+	   /*
+	    *@param: none
+		*@return: Llama al metodo para Printear la lista de Juegos 
+		*
+		*/
+	 
+	 public void PrintGames() {
+	        stock.PrintGames();
+	    }
+	 
+	 	/*
+	 	 * @param: void
+		*@return: Llama a un metodo para contar la cantidad de juegos de un determinado editor.
+		*
+		*/
+	 
+	 public boolean AmountGames() throws StockException, Exception {
+	        String publisher = Datos.recogeString(" Indica de que Editor quieres contar los Juegos ");
+	        //Falta introdocir el switch para elegir el editor que queremos contar.
+	        return stock.AmountGames(publisher);
+	    }
+	 
+	 /*
+	  * @param: none
+	  * @return: Llama al metodo que carga el archivo CSV en una lista
+	  * 
+	  */
+	 
+	 public void ChargeFileCVS() {
+	        //Por defecto
+	        stock.ChargeStock("");//Añadir Ruta y cambiar nombre de llamada.
+	    }
+	 
+
+	 /*
+	  * @param: none
+	  * @return: Llama al metodo que guarda la lista en un archivo CSV.
+	  * 
+	  */
+
+	    public void saveFileCSV() {
+	        //Por defecto
+	        stock.SaveStock(""); //Añadir Ruta y cambiar nombre de llamada.
+	    }
  
 }
