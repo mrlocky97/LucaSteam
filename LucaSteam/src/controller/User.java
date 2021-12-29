015d07b61312ebf1b43c84a2ec47cfe0e8e3bcca
alumno <alumno@Alumno10>
2021-12-29 18:02:44 +0100

    User, menu

--- LucaSteam/src/controller/User.java
+++ LucaSteam/src/controller/User.java
@@ -9,12 +9,16 @@ import utilidades.DataInput;
 public class User {
 
 	private ISuperServices services = new SuperServices();
+	
+	
+	
 	/**
 	*@param:none
 	*return: void
 	*/
     public void StartListGames() {
         boolean seguir = true;
+        start();
         do {
             Menu.showMenu(); //Muestra el menu
             seguir = this.chooseOptions(); //Entra dentro del metodo de abajo. El switch
@@ -29,64 +33,28 @@ public class User {
     public boolean chooseOptions() {
 
         boolean continuar = true;
-
+        
         try {
             switch (DataInput.writeNumber()) {//Lee un dato de tipo Int
                 case 1:
-                    //Cargar archivo en el programa y generar List (collection)
-                    services.loadList();
+                	// services.addGame();
+                    
+                    
                     break;
 
                 case 2:
-                    //Añadir un juego a la List
-                	// pasar por parametro un juego para añadir
-                    services.addGame();
+                	services.printGames();
+             
                     break;
 
                 case 3:
-                    //Mostrar la Lista de Juegos completa.
-                    services.printGames();
-                    break;
-
-                case 4:
-                    //Mostrar la Lista de Juegos filtrados por Nombre
-                 //   services.ShowGameFilterName();
-                    break;
-
-                case 5:
-                	//Mostrar la Lista de Juegos filtrados por Plataforma
-                  //  services.ShowGameFilterPlatform();
-                    break;
-
-                case 6:
-                	//Mostrar la Lista de Juegos filtrados por Año
-                  //  services.ShowGameFilterYear();
-                    break;
-
-                case 7:
-                	//Mostrar la Lista de Juegos filtrados por Genero
-                  //  services.ShowGameFilterGenre();
-                    break;
-
-                case 8:
-                	//Mostrar la Lista de Juegos filtrados por Editor
-               //     services.ShowGameFilterPublisher();
-                    break;
+                   //services.printByGenre();
                     
-                case 9:
-                	//Actualizar una categoria de un juego
-                  //  services.UpdateGame();
                     break;
-                   
-                case 10:
-                    //Borrar un Juego
-                  //  services.DeleteGame();
-                    break;
-                    
-                  //Guardar y salir
+
                 case 0:
-                //	services.SaveList();
-                    continuar = exit();
+                    continuar = false;
+                    
                     break;
             }
         } catch (Exception e) {
@@ -95,10 +63,10 @@ public class User {
         return continuar;
     }
 
-    private boolean exit() throws Exception {//ScanString lee un dato por Scanner tipo String
-        System.out.println("Estas seguro de que desea guardar y salir? (S/N)");
-    	String sino = DataInput.writeLine();
-        return (sino.toUpperCase().charAt(0) != 'S'); //La cambia a mayuscula
+   
+    
+    public void start() {
+    	services.loadList();
     }
 
 
