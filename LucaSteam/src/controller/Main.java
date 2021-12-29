4b82c28f009a46a389253bf871d3aea918d2173e
alumno <alumno@Alumno18>
2021-12-29 17:45:58 +0100

    New Game y Enum el SwtichPlatform

--- LucaSteam/src/controller/Main.java
+++ LucaSteam/src/controller/Main.java
@@ -1,10 +1,14 @@
 package controller;
 
+import model.Game;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Game game = new Game();
+		game.newGame();
 	}
 
 }
--- LucaSteam/src/utilidades/DataInput.java
+++ LucaSteam/src/utilidades/DataInput.java
@@ -5,19 +5,38 @@ import java.util.Scanner;
 public class DataInput {
 
 	public DataInput() {
-		
+
 	}
-	
-	@SuppressWarnings("resource")
+
 	public static String writeLine() {
-		String data= new Scanner(System.in).nextLine();
+		String data = "";
+		boolean noestaVacio = false;
+		while (!noestaVacio) {
+			data = new Scanner(System.in).nextLine();
+			if (data.trim().equals("")) {
+				System.out.println("El elemento esta vacio");
+			} else {
+				noestaVacio = true;
+			}
+		}
 		return data;
 	}
-	
+
 	@SuppressWarnings("resource")
 	public static int writeNumber() {
-		
-		int year = new Scanner(System.in).nextInt();
+		int year = -1;
+		boolean noesString = false;
+		while (!noesString) {
+			try {
+				// TODO: handle exception
+				System.out.println("Introduzca un num");
+				int data = new Scanner(System.in).nextInt();
+				noesString = true;
+				return data;
+			} catch (Exception e) {
+				System.out.println("Eso no es un num");
+			}
+		}
 		return year;
-}
+	}
 }
