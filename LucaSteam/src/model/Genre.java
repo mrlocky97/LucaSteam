f08fb5423202b0c661068624cbc4772967105102
juan sebastian quinayas guarin <sebastianquemasda@gmail.com>
2021-12-29 13:00:12 +0100

    modificado, enum genre y adaptado loadList a game

--- LucaSteam/src/model/Genre.java
+++ LucaSteam/src/model/Genre.java
@@ -1,57 +1,48 @@
 package model;
-   
+
 public enum Genre {
-	
-	PLATFORM("Platform"), SPORTS("Sports"), ROL("Role-Playing"), RACING("Racing"), 
-	MISC("Misc"), ACTION("Action"), SHOOTER("Shooter"), PUZZLE("Puzzle"),
-	SIMULATION("Simulation"), FIGHTING("Fighting"), STRATEGY("Strategy");
-	
-	
+
+	PLATFORM("Platform"), SPORTS("Sports"), ROL("Role-Playing"), RACING("Racing"), MISC("Misc"), ACTION("Action"),
+	SHOOTER("Shooter"), PUZZLE("Puzzle"), SIMULATION("Simulation"), FIGHTING("Fighting"), STRATEGY("Strategy");
+
 	private String g;
-	
 
 	private Genre(String g) {
 		this.g = g;
-		
-		
+
 	}
 
 	public String getGenre() {
 		return g;
 	}
 
+	private static Genre[] values = null;
 
-	 private static Genre[] values = null;
-	    
-	    /*
-	     * Esto va dentro de la clase game en metodo createGame 
-	     * 
-	     * this.Platform = Platform.switchGenre(Metodo para recoger int -> Datos.recogeInt());
-	     * 
-	     * */
-	 
-	 /*
-	  * @param String
-	  * 
-	  * @return 
-	  * 
-	  * */
-	 
-	 //Comprobar que no nos devuelve un null
-	 public static Genre switchPlatform(String val) {
+	/*
+	 * Esto va dentro de la clase game en metodo createGame
+	 * 
+	 * this.Platform = Platform.switchGenre(Metodo para recoger int ->
+	 * Datos.recogeInt());
+	 * 
+	 */
 
-			try {
-				return Genre.valueOf(val);
-			} catch (Exception e) {
-				e.getMessage();
-				return null;
-			}
+	/*
+	 * @param String
+	 * 
+	 * @return
+	 * 
+	 */
 
+	// Comprobar que no nos devuelve un null
+	public static Genre switchPlatform(String val) {
+		try {
+			return Genre.valueOf(val.toUpperCase());
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println("Error: El valor no existe....");
+			e.getMessage();
+			return null;
 		}
-	
-	
-	
+	}
 
 }
-	
-
